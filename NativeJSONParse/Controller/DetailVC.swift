2c0403eace0eee68fc832451cfb19facfe209f50
Peter Leung <ys.leung.uk@gmail.com>
2018-09-10 17:55:20 +0800

    Added the HTML code to make sure the content is being displayed nicely.

--- NativeJSONParse/Controller/DetailVC.swift
+++ NativeJSONParse/Controller/DetailVC.swift
@@ -21,7 +21,11 @@ class DetailVC: UIViewController {
     }
     
     func loadContent() {
-        let contentString = currentItem?.newsContent?.rendered ?? ""
-        contentWebView?.loadHTMLString(contentString, baseURL: URL(string: ""))
+        let content = currentItem?.newsContent?.rendered ?? ""
+        
+        //Added some HTML code at the top to make sure it loads correctly
+        let finalContentString = "<html><head><link rel='stylesheet' id='wpappbox-css' href='https://www.winandmac.com/wp-content/plugins/wp-appbox/css/styles.min.css?ver=3.4.6' type='text/css' media='screen'/><style type=\"text/css\">iframe {width:100%;height:auto}img {width:100%;height:auto}</style><meta name=\"viewport\" content=\"width=320px,user-scalable=no\" /><script type='text/javascript' src='https://platform.twitter.com/widgets.js'></script></head><body>\(content)</body></html>"
+        
+        contentWebView?.loadHTMLString(finalContentString, baseURL: URL(string: ""))
     }
 }
