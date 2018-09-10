f004b913206d3126f4516c911cab47a53b8c8a59
Peter Leung <ys.leung.uk@gmail.com>
2018-09-10 16:21:40 +0800

    Added a data handler to request data from the internet.

---
+++ NativeJSONParse/Model/DataHandler.swift
@@ -0,0 +1,33 @@
+//
+//  DataModel.swift
+//  NativeJSONParse
+//
+//  Created by Peter Leung on 10/9/2018.
+//  Copyright © 2018 Peter Leung. All rights reserved.
+//
+
+import Foundation
+
+class DataHandler {
+    
+    //MARK: Function to start to request data from the internet
+    func requestData(completion: @escaping ((_ newsItems: [NewsItem]) -> Void)) {
+        
+        let jsonUrlString = "https://www.winandmac.com/wp-json/wp/v2/posts/?per_page=10"
+        
+        guard let url = URL(string: jsonUrlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else { return }
+            
+            do {
+                let news = try JSONDecoder().decode([NewsItem].self, from: data)
+                completion(news)
+            } catch let jsonError {
+                print("error \(jsonError.localizedDescription)")
+            }
+            
+            }.resume()
+    }
+}
